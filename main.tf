e72c121323d5d0d9549a2d2f4ecb67cc50322ac2
comeanother <const.bon@gmail.com>
2017-08-10 20:51:38 +0300

    Define precise ACLs

--- main.tf
+++ main.tf
@@ -29,7 +29,8 @@ data "aws_iam_policy_document" "ami_backup" {
 
   statement {
     actions = [
-      "ec2:DescribeInstances"
+      "ec2:DescribeInstances",
+      "ec2:CreateImage"
     ]
 
     resources = [
